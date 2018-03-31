a5a029c98010a08fc4a0f7f9b12b6e1adb3774e2
serjay88 <serjay88@gmail.com>
2018-03-31 23:58:01 +0300

    Change backend path

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 export default {
   API_URI:
     process.env.NODE_ENV === 'production'
-      ? 'https://hasky-react-chat.herokuapp.com/'
+      ? 'https://dogecodes-chat-api.herokuapp.com/v1'
       : 'http://localhost:8000/v1',
   SOCKETS_URI:
     process.env.NODE_ENV === 'production'
-      ? 'wss://hasky-react-chat.herokuapp.com/'
+      ? 'wss://dogecodes-chat-api.herokuapp.com/'
       : 'ws://localhost:8000/',
 };
